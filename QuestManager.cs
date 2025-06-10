2f6b8e4ad3e22c82a8104c633238447106734518
Antonio Labinjan <alabinjan6@gmail.com>
2025-06-10 10:41:59 +0200

    Update QuestManager.cs
    added whacky camera shake when player does smthng wrong

--- QuestManager.cs
+++ QuestManager.cs
@@ -1,4 +1,3 @@
-// For Plant watering quest
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +6,27 @@ public class QuestManager : MonoBehaviour
 {
     public List<Plant> plants;
 
+    public Camera playerCamera; 
+
     private int currentIndex = 0;
 
     void Start()
     {
         plants.Sort((a, b) => a.height.CompareTo(b.height));
         Debug.Log("[QuestManager] Quest pocinje. Zalivaj biljke od najnize do najvise.");
+
+        if (playerCamera == null)
+        {
+            playerCamera = Camera.main;
+        }
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
         {
-            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            if (Physics.Raycast(ray, out RaycastHit hit, 4f)) // 4f = maksimalna udaljenost
+            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, 4f))
             {
                 Plant lookedAtPlant = hit.collider.GetComponent<Plant>();
                 if (lookedAtPlant != null && !lookedAtPlant.isWatered)
@@ -30,8 +36,6 @@ public class QuestManager : MonoBehaviour
             }
         }
     }
-
-
 
     void TryWaterPlant(Plant plant)
     {
@@ -50,24 +54,47 @@ public class QuestManager : MonoBehaviour
         else
         {
             Debug.Log("[QuestManager] Krivi redoslijed! Pokusaj ponovno.");
-            ResetQuest();
+            StartCoroutine(SimpleScaryEffect());
         }
     }
 
-    void ResetQuest()
+    IEnumerator SimpleScaryEffect()
     {
+        Debug.Log("[QuestManager] Mrak te proguta... Pogrijesio/la si.");
+
+        float blinkDuration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < blinkDuration)
+        {
+            if (playerCamera != null)
+                playerCamera.enabled = false;
+
+            yield return new WaitForSeconds(0.1f);
+
+            if (playerCamera != null)
+                playerCamera.enabled = true;
+
+            yield return new WaitForSeconds(0.2f);
+
+            elapsed += 0.3f; // 0.1 + 0.2
+        }
+
+        // Reset biljki
         currentIndex = 0;
         foreach (var p in plants)
         {
             p.isWatered = false;
         }
+
         Debug.Log("[QuestManager] Quest resetiran.");
     }
 
+
     IEnumerator OnQuestComplete()
     {
-        Debug.Log("[QuestManager] Quest završio: You should leave hotel.");
+        Debug.Log("[QuestManager] Quest zavrsio: You should leave hotel.");
         yield return new WaitForSeconds(3f);
-        // Daljnja logika nakon završetka questa
+        // Daljnja logika nakon zavrsetka questa
     }
 }
